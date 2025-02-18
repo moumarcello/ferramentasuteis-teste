419fdf415ab60e423cce0e1de353b0b202b186b6
Marcello Moura <moumarcello@gmail.com>
2025-02-18 13:50:17 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,54 @@
-// // Numeros randomicos:--------------------------------------
+// Senha específica sequencial:--------------------------------------
+
+function gerarEsp2() {
+    let charsConsoantes = "bcdfghjklmnpqrstvwxyz"
+    let charsVogais = "aeiou"
+    let charsNum = "0123456789"
+
+    let tamanhoSenhaNum2 = 2
+
+    // Letras
+    let primeiraLetra = charsConsoantes[Math.floor(Math.random() * charsConsoantes.length)].toUpperCase()
+    let letraDoMeio = charsVogais[Math.floor(Math.random() * charsVogais.length)]
+    let terceiraLetra = charsConsoantes[Math.floor(Math.random() * charsConsoantes.length)]
+    let senhaLetra2 = primeiraLetra + letraDoMeio + terceiraLetra
+
+    let senhaFinal = senhaLetra2 + senhaLetra2.toLowerCase() + senhaLetra2.toLowerCase() + senhaLetra2.toLowerCase() + senhaLetra2.toLowerCase()
+
+    // Números
+    let senhaNum2 = "";
+    for (let i = 0; i < tamanhoSenhaNum2; i++) {
+        let aleatorioNum = Math.floor(Math.random() * charsNum.length)
+        senhaNum2 += charsNum[aleatorioNum]
+    }
+    
+    resEsp2.innerHTML = senhaFinal + senhaNum2
+}
+
+// Senha específica:--------------------------------------
+
+function gerarEsp() {
+    let charsOiLetra = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJLMNOPQRSTUVWXYZ"
+    let tamanhoSenhaLetra = 14
+
+    let charsOiNum = "0123456789"
+    let tamanhoSenhaNum = 2
+
+    let senhaLetra = ""
+    let senhaNum = ""
+    
+    for (let i = 0; i < tamanhoSenhaLetra; i++) {
+        let aleatorio = Math.floor(Math.random() * charsOiLetra.length)
+        senhaLetra += charsOiLetra.substring(aleatorio, aleatorio + 1)
+    }
+    for (let i = 0; i < tamanhoSenhaNum; i++) {
+        let aleatorioNum = Math.floor(Math.random() * charsOiNum.length)
+        senhaNum += charsOiNum.substring(aleatorioNum, aleatorioNum + 1)
+    }
+    resOi.innerHTML = senhaLetra + senhaNum
+}
+
+// Numeros randomicos:--------------------------------------
 let resultado = document.getElementById('res')
 
 function gerar4() {
@@ -56,27 +106,3 @@ function gerar10char() {
     }
     resultadoSenha.innerHTML = senha
 }
-
-// Senha específica:--------------------------------------
-
-function gerarEsp() {
-    let charsOiLetra = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJLMNOPQRSTUVWXYZ"
-    let tamanhoSenhaLetra = 14
-
-    let charsOiNum = "0123456789"
-    let tamanhoSenhaNum = 2
-
-    let senhaLetra = ""
-    let senhaNum = ""
-    
-    for (let i = 0; i < tamanhoSenhaLetra; i++) {
-        let aleatorio = Math.floor(Math.random() * charsOiLetra.length)
-        senhaLetra += charsOiLetra.substring(aleatorio, aleatorio + 1)
-    }
-    for (let i = 0; i < tamanhoSenhaNum; i++) {
-        let aleatorioNum = Math.floor(Math.random() * charsOiNum.length)
-        senhaNum += charsOiNum.substring(aleatorioNum, aleatorioNum + 1)
-    }
-    resOi.innerHTML = senhaLetra + senhaNum
-}
-
